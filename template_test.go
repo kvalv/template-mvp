00debe000d0bf65fdcaeb1129f5d8a42e5a76bac
mikael kvalvaer <mikaelkvalvaer@gmail.com>
2025-02-09 14:34:22 +0100

    test case

--- template_test.go
+++ template_test.go
@@ -7,18 +7,41 @@ import (
 )
 
 func TestTemplate(t *testing.T) {
-	templ := template.New("Hello {{.Name}}")
-
-	data := struct {
-		Name string
-	}{Name: "World"}
-
-	got, err := templ.Parse(&data)
-	if err != nil {
-		t.Fatalf("Parse error: %s", err)
+	cases := []struct {
+		descr string
+		input string
+		data  any
+		want  string
+	}{
+		{
+			descr: "simple",
+			input: "Hello {{.Name}}",
+			data: struct {
+				Name string
+			}{Name: "World"},
+			want: "Hello World",
+		},
+		{
+			descr: "two variables",
+			input: "One {{.Two}} {{.Three}}",
+			data: struct {
+				Two   string
+				Three string
+			}{Two: "Two", Three: "Three"},
+			want: "One Two Three",
+		},
 	}
-	want := "Hello World"
-	if want != got {
-		t.Fatalf("Result mismatch; want=%q, got=%q", want, got)
+
+	for _, tc := range cases {
+		t.Run(tc.descr, func(t *testing.T) {
+			templ := template.New(tc.input)
+			got, err := templ.Parse(&tc.data)
+			if err != nil {
+				t.Fatalf("Parse error: %s", err)
+			}
+			if tc.want != got {
+				t.Fatalf("Result mismatch; want=%q, got=%q", tc.want, got)
+			}
+		})
 	}
 }
